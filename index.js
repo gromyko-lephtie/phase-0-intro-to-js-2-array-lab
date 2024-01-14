6fadb964d1c158a604ea5fa250cc3777db07980d
+ <+>
2024-01-14 21:02:02 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Yoyo) {
+    cats.push(Yoyo);
+}
+
+function destructivelyPrependCat(vader) {
+    cats.unshift(vader);
+}
+
+function destructivelyRemoveLastCat(Fluffy) {
+    cats.pop(Fluffy);
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+}
+
+function appendCat() {
+    return cats.concat('Broom');
+}
+
+const prependCat = () => {
+    return ['Arnold', ...cats];
+};
+
+const removeLastCat = () => {
+    return cats.slice(0, -1);
+};
+
+const removeFirstCat = () => {
+    return cats.slice(1);
+};
